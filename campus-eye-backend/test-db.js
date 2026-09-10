9cf1579774b9dd7057744e712a43e135c6a1c1c7
tjidev <joshtimchen@gmail.com>
2026-09-10 22:22:37 +0530

    delete testdb

--- campus-eye-backend/test-db.js
+++
@@ -1,9 +0,0 @@
-const db = require("./database");
-
-// Check users table
-const users = db.prepare("SELECT * FROM users").all();
-console.log("Users:", users);
-
-// Check complaints table
-const complaints = db.prepare("SELECT * FROM complaints").all();
-console.log("Complaints:", complaints);
